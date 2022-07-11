68dd716c424e17bdc772ecc982589cd602824c1b
Rehan Haider <email@rehanhaider.com>
2022-07-11 20:00:36 +0000

    FEAT: HTML parsing to get href complete

--- pyHTMLProofer/__init__.py
+++ pyHTMLProofer/__init__.py
@@ -8,64 +8,6 @@ from argparse import ArgumentError
 from pyHTMLProofer.Runner import Runner
 
 
-def check_file(file_path, options=None):
-    """
-    Check the file for internal & external links
-    """
-    # Raise error if file path is not a string
-    if not isinstance(file_path, str):
-        raise ArgumentError("File path must be a string")
-
-    # Raise error if the file does not exist
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(f"File does not exist: {file_path}")
-
-    options["type"] = "file"
-
-    return Runner(file_path, options)
-
-
-def check_directory(directory_path, options=None):
-    """
-    Check the directory for internal & external links
-    :param directory_path:
-    :return:
-    """
-
-    # Raise error if directory path is not a string
-    if not isinstance(directory_path, str):
-        raise ArgumentError("Directory path must be a string")
-
-    # Raise error if the directory does not exist
-    if not os.path.isdir(directory_path):
-        raise FileNotFoundError(f"Directory does not exist: {directory_path}")
-
-    options["type"] = "directory"
-
-    return Runner(directory_path, options)
-
-
-def check_directories(directories, options=None):
-    """
-    Check the directories for internal & external links
-    :param directory_paths:
-    :return:
-    """
-
-    # Raise error if directory path is not a string
-    if not isinstance(directories, list):
-        raise ArgumentError("Directory paths must be a list")
-
-    # Raise error if the directory does not exist
-    for directory in directories:
-        if not os.path.isdir(directory):
-            raise FileNotFoundError(f"Directory does not exist: {directory}")
-
-    options["type"] = "directories"
-
-    return Runner(directories, options)
-
-
 def check_links(links, options=None):
     """
     Check the links for internal & external links
---
+++ pyHTMLProofer/utils/URL.py
@@ -0,0 +1,44 @@
+"""This class validates URLs format and segregates them into internal and external URLs."""
+import requests
+from bs4 import BeautifulSoup
+
+
+class _URL:
+    def __init__(self, url, options=None):
+        self.url = url
+        self.options = options
+        self.type = self._get_type()
+        self.html_soup = None
+
+    def validate(self):
+        """
+        This method is used to validate URLs.
+        """
+
+        if self.type == "external":
+            return self._validate_external_url()
+
+    def _validate_external_url(self):
+        """
+        This method is used to validate external URLs.
+        """
+        try:
+            response = requests.get(self.url)
+            if response.status_code != 200:
+                return False
+            self.html_soup = BeautifulSoup(response.text, "html5lib")
+            return True
+        except Exception:
+            return False
+
+    def _validate_internal_url(self):
+        """
+        This method is used to validate internal URLs.
+        """
+        pass
+
+    def _get_type(self):
+        """
+        This method is used to get the type of URL.
+        """
+        return "external" if self.url.startswith("http") else "internal"
