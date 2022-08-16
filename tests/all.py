7ffdef60b0caa26c10e918c2db199d0015af1753
Rehan Haider <email@rehanhaider.com>
2022-08-16 19:32:17 +0000

    Added PDM script to test manual test execution

--- tests/all.py
+++ tests/all.py
@@ -1,8 +1,8 @@
 import pyHtmlProofer
 
 # Disable write to file
+def main():
+    options = {"report_to_file": False}
+    file_path = ["tests/cases/5/"]
 
-options = {"report_to_file": False}
-file_path = ["tests/cases/5/"]
-
-pyHtmlProofer.directories(file_path, options=options).check()
+    pyHtmlProofer.directories(file_path, options=options).check()
