3fe76d86b79e86855b055fdb52b8499ba8e3a9b8
Rehan Haider <email@rehanhaider.com>
2022-08-24 15:37:03 +0000

    FIX: Fixed incorrect CLI configuration

--- src/pyhtmlproofer/__version__.py
+++ src/pyhtmlproofer/__version__.py
@@ -1,4 +1,4 @@
 """Version of pyHTMLProofer."""
 from __future__ import unicode_literals
 
-__version__ = "0.7.1-alpha"
+__version__ = "0.7.2-alpha"
--- tests/all.py
+++ tests/all.py
@@ -3,6 +3,10 @@ import pyhtmlproofer
 # Disable write to file
 def main():
     options = {"report_to_file": False}
-    file_path = ["tests/cases/"]
+    file_path = ["tests/cases/5/"]
 
     pyhtmlproofer.directories(file_path, options=options).check()
+
+
+if __name__ == "__main__":
+    main()
